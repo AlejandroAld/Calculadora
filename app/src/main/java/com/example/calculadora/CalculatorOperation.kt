a1ebcaebfea39f8b74d539955dcccb3f21fa2f73
AlejandroAld <jaldamaramos@gmail.com>
2023-09-08 12:12:01 -0600

    Inicio

---
+++ app/src/main/java/com/example/calculadora/CalculatorOperation.kt
@@ -0,0 +1,8 @@
+package com.example.calculadora
+
+sealed class CalculatorOperation(val symbol: String) {
+    object Add : CalculatorOperation(symbol = "+")
+    object Subtract : CalculatorOperation(symbol = "-")
+    object Multiply : CalculatorOperation(symbol = "x")
+    object Divide : CalculatorOperation(symbol = "/")
+}
